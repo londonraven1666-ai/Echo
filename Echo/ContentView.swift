3c7be3dbe268fafd45a727e08c61eb5e234f2178
Altair <altair@zuhause.local>
2026-05-14 03:29:12 -0700

    fix: move Color extension into ContentView

--- Echo/ContentView.swift
+++ Echo/ContentView.swift
@@ -67,3 +67,16 @@ struct ConfigWebView: UIViewRepresentable {
 }
 
 import WebKit
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        self.init(
+            red: Double((rgbValue >> 16) & 0xFF) / 255,
+            green: Double((rgbValue >> 8) & 0xFF) / 255,
+            blue: Double(rgbValue & 0xFF) / 255
+        )
+    }
+}
